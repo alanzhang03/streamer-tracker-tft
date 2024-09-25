358b5b3e24ccc932a2c0f1e82123f22b2ec1032e
alanzhang03 <alan.s.zhang@gmail.com>
2024-09-25 19:28:39 -0400

    background color

--- frontend/src/app/NavbarMain.jsx
+++ frontend/src/app/NavbarMain.jsx
@@ -13,7 +13,7 @@ const NavbarMain = () => {
 				</ul>
 				<ul className="navbar-menu">
 					<li>
-						<Link href="/">About</Link>
+						<Link href="/about">About</Link>
 					</li>
 					<li>
 						<Link href="/streamers">Streamers</Link>
