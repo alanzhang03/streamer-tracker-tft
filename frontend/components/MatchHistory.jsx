4c397db31dda281adb7f959605a1f74da59bf235
alanzhang03 <alan.s.zhang@gmail.com>
2024-09-27 22:50:53 -0400

    navbar dropdown

--- frontend/components/MatchHistory.jsx
+++ frontend/components/MatchHistory.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./styles/MatchHistory.scss";
 
-const MatchHistory = () => {
-  return (
-    <div>MatchHistory</div>
-  )
-}
+const MatchHistory = ({ matchHistory = [] }) => {
+	return (
+		<div className="match-history">
+			<h3>Match History</h3>
+			<ul>
+				{matchHistory.length > 0 ? (
+					matchHistory.map((match, index) => (
+						<li key={index} className="match-item">
+							{match}
+						</li>
+					))
+				) : (
+					<li className="match-item">No match history available</li>
+				)}
+			</ul>
+		</div>
+	);
+};
 
-export default MatchHistory
+export default MatchHistory;
--- frontend/src/app/NavbarMain.jsx
+++ frontend/src/app/NavbarMain.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import "./NavbarMain.scss";
 import Link from "next/link";
 
 const NavbarMain = () => {
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+	const handleMouseEnter = () => {
+		setIsDropdownOpen(true);
+	};
+
+	const handleMouseLeave = () => {
+		setIsDropdownOpen(false);
+	};
+
 	return (
 		<nav className="navbar">
 			<div className="navbar-container">
@@ -13,21 +25,30 @@ const NavbarMain = () => {
 				</ul>
 				<ul className="navbar-menu">
 					<li>
+						<Link href="/">Home</Link>
+					</li>
+					<li>
 						<Link href="/about">About</Link>
 					</li>
-					<li>
+					<li
+						className="dropdown"
+						onMouseEnter={handleMouseEnter}
+						onMouseLeave={handleMouseLeave}
+					>
 						<Link href="/streamers">Streamers</Link>
-					</li>
-					<li>
-						<Link href="/streamers/Dishsoap">Dishsoap</Link>
-					</li>
-
-					<li>
-						<Link href="/streamers/Setsuko">Setsuko</Link>
-					</li>
-
-					<li>
-						<Link href="/streamers/K3soju">K3soju</Link>
+						{isDropdownOpen && (
+							<ul className="dropdown-content">
+								<li>
+									<Link href="/streamers/Dishsoap">Dishsoap</Link>
+								</li>
+								<li>
+									<Link href="/streamers/Setsuko">Setsuko</Link>
+								</li>
+								<li>
+									<Link href="/streamers/K3soju">K3soju</Link>
+								</li>
+							</ul>
+						)}
 					</li>
 				</ul>
 			</div>
