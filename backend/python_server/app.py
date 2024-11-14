c1a9682b663e5ff4a80674e4c79acd0ccf821596
charlielu04 <charleslu04@gmail.com>
2024-11-14 10:18:05 -0500

    api created
    to use, get into python_server folder and run "python app.py" in console. server will be running and you can use the "/api/match-history" endpoint to get match history of a player

--- backend/python_server/app.py
+++ backend/python_server/app.py
@@ -1,67 +1,63 @@
 from flask import Flask, request, jsonify
-
-# import requests
-# import os
-# from psycopg2 import pool
-# from dotenv import load_dotenv
-# from psycopg2.extras import Json, DictCursor
-# import time
+from functools import reduce
+import os
+from psycopg2 import pool
+from dotenv import load_dotenv
+from psycopg2.extras import Json, DictCursor
 
 app = Flask(__name__)
 
-# api_key = 'RGAPI-314e77e7-1963-4196-96b5-cf8c8d0777b9'
-# streamers = ['e62ZthNQBc1bjVfXrjDvNIQV-0Bd7GUeaRv_fiZqx5nc5OXaY6OAmM4wav043AJgxKQShtA1s9IIOg', 'hVi-4fc19ymaRf4HvUmHNkZCoALJPUXgGKcKGgBlLZTqYZRVtQwglGQCIBj0jou0wA35CcH8sdeEgw', 'NUnja2Bl0CPRiimqr1PesnOnDsxXa6eAg5xRhNVBdxgAzBopnpcWNTZZ9r9YjNhEtf5tx2DkvXdJ9Q']
+api_key = 'RGAPI-5db6b275-4ebd-4b03-9b74-aa0b0d307e20'
+
+# takes in a username and returns the most recent matches from page x, pages starting at 0
+def getStreamerData(username, page):
+    page = int(page)
+    page_size = 5
+    load_dotenv()
+
+    # Get the connection string from the environment variable
+    connection_string = os.getenv('DATABASE_URL')
+
+    # Create a connection pool
+    connection_pool = pool.SimpleConnectionPool(
+        1,  # Minimum number of connections in the pool
+        10,  # Maximum number of connections in the pool
+        connection_string
+    )
+
+    # Check if the pool was created successfully
+    if connection_pool:
+        print("Connection pool created successfully")
+
+    # Get a connection from the pool
+    conn = connection_pool.getconn()
+    conn.autocommit = True
+
+    # Create a cursor object
+    cur = conn.cursor(cursor_factory=DictCursor)
+
+    cur.execute("SELECT match_data FROM matches WHERE '%s' = ANY(players) ORDER BY game_datetime DESC" % (username))
+    res = cur.fetchall()
+    cur.close()
+    connection_pool.putconn(conn)
+
+    # Close all connections in the pool
+    connection_pool.closeall()
+
+    
+    return reduce(lambda x,y :x+y , res[page*5:(page+1)*5])
 
 
-# def getStreamerData():
-#     load_dotenv()
-
-#     # Get the connection string from the environment variable
-#     connection_string = os.getenv('DATABASE_URL')
-
-#     # Create a connection pool
-#     connection_pool = pool.SimpleConnectionPool(
-#         1,  # Minimum number of connections in the pool
-#         10,  # Maximum number of connections in the pool
-#         connection_string
-#     )
-
-#     # Check if the pool was created successfully
-#     if connection_pool:
-#         print("Connection pool created successfully")
-
-#     # Get a connection from the pool
-#     conn = connection_pool.getconn()
-#     conn.autocommit = True
-
-#     # Create a cursor object
-#     cur = conn.cursor(cursor_factory=DictCursor)
-
-#     cur.execute('SELECT 5 FROM matches WHERE match_id=%s', (match, ))
-
-#     cur.close()
-#     connection_pool.putconn(conn)
-
-#     # Close all connections in the pool
-#     connection_pool.closeall()
-
-
-
-data_store = []
-# 
-@app.route('/api/data', methods=['GET'])
+# get the match history and return a list of matches in json format
+@app.route('/api/match-history', methods=['GET'])
 def get_data():
-    return jsonify(data_store)
+    try:
+        user = request.headers['username-tagline']
+        pagenum = request.headers['page-number']
+    except:
+        return jsonify("problem with header one or both of username-tagline or page-number")
 
-# Route to handle POST requests
-@app.route('/api/data', methods=['POST'])
-def add_data():
-    data = request.get_json()  # Get JSON data from the request
-    if data:
-        data_store.append(data)  # Add new data to the data store
-        return jsonify({'message': 'Data added successfully!'}), 201
-    else:
-        return jsonify({'error': 'Invalid data format'}), 400
+    return getStreamerData(user, pagenum)
 
 # Home route to verify the server is running
 @app.route('/')
