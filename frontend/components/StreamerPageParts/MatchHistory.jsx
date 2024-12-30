dba3378db666ca2c15a505cb49d351e0d42a4fb6
alanzhang03 <107202923+alanzhang03@users.noreply.github.com>
2024-12-30 19:05:06 +0800

    added

--- frontend/components/StreamerPageParts/MatchHistory.jsx
+++ frontend/components/StreamerPageParts/MatchHistory.jsx
@@ -9,17 +9,28 @@ const MatchHistory = ({ data, images }) => {
     <div className="match-history">
       {data.map((match, index) => {
         const player = match["Player 0"];
+        const timestamp = match.game_datetime;
 
         return (
           <div key={index} className="match-card">
             <div className="match-header">
               <div className="header-info">
-                <h2 style={{ color: determineColor(player.placement) }}>
-                  Placement: {player.placement}
-                </h2>
-                <p>+{player.lp_gain || 0} LP</p>
-                <p>Gold Left: {player.gold_left}</p>
-                <p>Level: {player.level}</p>
+                <div className="header-info-row-1">
+                  <h2 style={{ color: determineColor(player.placement) }}>
+                    Placement: {player.placement}
+                  </h2>
+                  <p style={{ color: player.lp_gain >= 0 ? "green" : "red" }}>
+                    {player.lp_gain >= 0
+                      ? `+${player.lp_gain}`
+                      : `${player.lp_gain}`}{" "}
+                    LP
+                  </p>
+                  <p>Gold Left: {player.gold_left}</p>
+                  <p>Level: {player.level}</p>
+                </div>
+                <div className="header-info-row-2">
+                  <p className="get-time-stamp">{getTimeAgo(timestamp)}</p>
+                </div>
               </div>
 
               <Augments playerTraits={player.traits} />
@@ -61,4 +72,23 @@ function determineColor(placement) {
     default:
       return "Grey";
   }
+}
+
+function getTimeAgo(timestamp) {
+  const matchDate = new Date(timestamp);
+  const now = new Date();
+  const differenceInTime = now - matchDate;
+  const differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
+
+  if (differenceInDays < 1) {
+    const differenceInHours = Math.floor(differenceInTime / (1000 * 3600));
+    if (differenceInHours < 1) {
+      const differenceInMinutes = Math.floor(differenceInTime / (1000 * 60));
+      return differenceInMinutes < 1
+        ? "Just now"
+        : `${differenceInMinutes} minutes ago`;
+    }
+    return `${differenceInHours} hours ago`;
+  }
+  return `${differenceInDays} days ago`;
 }
--- frontend/src/app/NavbarMain.jsx
+++ frontend/src/app/NavbarMain.jsx
@@ -5,56 +5,56 @@ import "./NavbarMain.scss";
 import Link from "next/link";
 
 const NavbarMain = () => {
-	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-	const handleMouseEnter = () => {
-		setIsDropdownOpen(true);
-	};
+  const handleMouseEnter = () => {
+    setIsDropdownOpen(true);
+  };
 
-	const handleMouseLeave = () => {
-		setIsDropdownOpen(false);
-	};
+  const handleMouseLeave = () => {
+    setIsDropdownOpen(false);
+  };
 
-	return (
-		<nav className="navbar">
-			<div className="navbar-container">
-				<ul>
-					<Link className="nav-label-left" href="/">
-						LOGO Streamer Tracker TFT
-					</Link>
-				</ul>
-				<ul className="navbar-menu">
-					<li>
-						<Link href="/">Home</Link>
-					</li>
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul>
+          <Link className="nav-label-left" href="/">
+            LOGO Streamer Tracker TFT
+          </Link>
+        </ul>
+        <ul className="navbar-menu">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
 
-					<li
-						className="dropdown"
-						onMouseEnter={handleMouseEnter}
-						onMouseLeave={handleMouseLeave}
-					>
-						<Link href="#streamers">Streamers</Link>
-						{isDropdownOpen && (
-							<ul className="dropdown-content">
-								<li>
-									<Link href="/streamers/Dishsoap">Dishsoap</Link>
-								</li>
-								<li>
-									<Link href="/streamers/Setsuko">Setsuko</Link>
-								</li>
-								<li>
-									<Link href="/streamers/K3soju">K3soju</Link>
-								</li>
-							</ul>
-						)}
-					</li>
-					<li>
-						<Link href="/about">About</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+          <li
+            className="dropdown"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Link href="#streamers">Streamers</Link>
+            {isDropdownOpen && (
+              <ul className="dropdown-content">
+                <li>
+                  <Link href="/streamers/K3soju">K3soju</Link>
+                </li>
+                <li>
+                  <Link href="/streamers/Setsuko">Setsuko</Link>
+                </li>
+                <li>
+                  <Link href="/streamers/Dishsoap">Dishsoap</Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavbarMain;
